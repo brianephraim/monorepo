9f404d2ceff5a28250d3eefe1f66249653749ca4
Brian Ephraim <brianephraim@gmail.com>
2017-06-02 10:02:27 -0400

    x

---
+++ packages/aaa4/aaa4.js
@@ -0,0 +1,4 @@
+// repo name: aaa4
+// npm name: @defualt/aaa4
+console.log('hey there from aaa4 -- @defualt/aaa4');
+export default 'aaa4 -- @defualt/aaa4';
---
+++ packages/aaa4/aaa4.test.js
@@ -0,0 +1,5 @@
+// import aaa4 from ./aaa4;
+// const aaa4 = require('./aaa4');
+test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
