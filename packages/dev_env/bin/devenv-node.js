7f8e83960cbf005200919bdcb525fc4dde57a945
Brian Ephraim <brianephraim@gmail.com>
2017-06-17 08:30:49 -0400

    X

--- packages/dev_env/bin/devenv-node.js
+++ packages/dev_env/bin/devenv-node.js
@@ -2,9 +2,8 @@
 
 const path = require('path');
 const shellCommand = require('../pre-webpack/shell-command');
-console.log(process.argv.slice(3))
+
 const toCompile = path.resolve(process.cwd(), process.argv[2]);
-console.log('dddd',process.argv.indexOf('new'));
 const toCompileSplit = toCompile.split('/');
 const fileName = toCompileSplit.pop();
 const tempFileName = fileName.replace('.js', '.temp.js');
@@ -17,7 +16,7 @@ const babelNodePath = path.resolve(__dirname, babelNodePathSpecific);
 
 const babelStartScript = path.resolve(__dirname, '../pre-webpack/babel.start.js');
 
-const cmd = `(cd ~/junk && ${babelNodePath} ${babelStartScript} --entry=${toCompile} --output=${tempFilePath}) && node ${tempFilePath} ${process.argv.slice(3).join(' ')} && rm ${tempFilePath} && rm ${tempFilePath}.map`;
+const cmd = `(cd ${toCompileFolder} && ${babelNodePath} ${babelStartScript} --entry=${toCompile} --output=${tempFilePath}) && node ${tempFilePath} ${process.argv.slice(3).join(' ')} && rm ${tempFilePath} && rm ${tempFilePath}.map`;
 
 shellCommand(cmd);
 
