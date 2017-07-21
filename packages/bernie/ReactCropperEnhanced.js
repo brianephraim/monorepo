1686b098ca0a707d6c2c5c8b6bc9b7eef669ffbd
Brian Ephraim <brianephraim@gmail.com>
2017-07-20 20:32:03 -0400

    asfdas

--- packages/bernie/ReactCropperEnhanced.js
+++ packages/bernie/ReactCropperEnhanced.js
@@ -12,13 +12,13 @@ import vanilla from 'vanilla';
 //  1. drag crop area outside canvas
 //    - importing `cropperLibMonkeyPatch` solves this
 //  2. template overlay of cropper area
-//    - this is accomplished with the DOM included manipulation below
+//    - this is accomplished with the DOM manipulation included below
 //  3. screen resize re-inits cropper (so cropper resizes appropriately)
 //    - this is accomplished with the component lifecycle callbacks below
 
 // Note
 // There's some hoops jumped through due to the library dispatching crop events
-// on resize.  So bindings on the `onmove` and `onend` are used to circumvent
+// on resize.  So bindings on the `onmove` and `onend` are used to circumvent a
 // crop-dispatch-on-resize circumstance.
 
 class ReactCropperEnhanced extends Component {
@@ -31,7 +31,6 @@ class ReactCropperEnhanced extends Component {
     this._cropBound = this._crop.bind(this);
     this._cropmoveBound = this._cropmove.bind(this);
     this._cropendBound = this._cropend.bind(this);
-    this.i = 0;
   }
   componentDidMount() {
     // When window resizes, flicker cropperExists.
@@ -45,16 +44,12 @@ class ReactCropperEnhanced extends Component {
     });
   }
   componentWillUnmount() {
-    // cleanup
     if (this.windowSizerCb) {
-      windowSizer.removeCb();
+      windowSizer.removeCb(this.windowSizerCb);
       delete this.windowSizerCb;
     }
   }
   _ready(...args){
-    // cropOverlayImg implementation
-    // DOM manipulation because core cropperjs lib is DOM based
-    //'http://s3-us-west-1.amazonaws.com/bernieapp/decorations/h3.png'
     if (
       this.props.cropSrc &&
       this.refs &&
@@ -62,50 +57,38 @@ class ReactCropperEnhanced extends Component {
       this.refs.cropper.cropper &&
       this.refs.cropper.cropper.cropBox
     ) {
+      // cropOverlayImg implementation
+      // DOM manipulation because core cropperjs lib is DOM based
+      //'http://s3-us-west-1.amazonaws.com/bernieapp/decorations/h3.png'
       const sampleImg = document.createElement('img');
       sampleImg.setAttribute('src', this.props.cropSrc);
       sampleImg.setAttribute('style', 'position: absolute; width: 100%; height: 100%;');
       vanilla.prepend(this.refs.cropper.cropper.cropBox, sampleImg);
+
       // because we are resetting the cropper for the sake of resizing
       // we need to re-establish any cropping the user had done previously
       if (this.state.lastCropData) {
-        console.log('READY this.state.lastCropData',this.state.lastCropData);
         this.refs.cropper.cropper.setData(this.state.lastCropData);
       }
     }
-    console.log(this.i++);
-    
 
-    //windowSizer.dimensions.width
-
-    // passthrough of `ready` prop
     this.props.ready && this.props.ready(...args);
-
-    window.xx = this.refs.cropper;
   }
   _crop(cropData, ...args){
-    console.log('CROP')
     if (this.state.moving) {
-
       this.setState({
         lastCropData: cropData.detail,
       });
     }
-
-    // passthrough of `crop` prop
     this.props.crop && this.props.crop(cropData, ...args);
   }
 
   _cropend(...args){
-    console.log('CROPEND')
     this.setState({moving: false});
-    // this.moving = false;
     this.props.cropend && this.props.cropend(...args);
   }
   _cropmove(...args){
-    console.log('CROPMOVE')
     this.setState({moving: true});
-    // this.moving = true;
     this.props.cropmove && this.props.cropmove(...args);
   }
   render() {
