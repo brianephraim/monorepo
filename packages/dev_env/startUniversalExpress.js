dccfc449ccb2ebf6c077b273118435802b6b8921
Brian Ephraim <brianephraim@gmail.com>
2017-09-27 13:57:29 -0400

    deploy attempt

--- packages/dev_env/startUniversalExpress.js
+++ packages/dev_env/startUniversalExpress.js
@@ -37,7 +37,7 @@ export default function startUniversal(app) {
       const multiCompiler = webpack([clientDevConfig, isUniversal ? serverDevConfig : serverNonUniversalConfig])
       const clientCompiler = multiCompiler.compilers[0]
       /* eslint-disable no-console */
-      console.info('🔷 Starting webpack ...');
+      console.info(`🔷 Starting webpack development for ${isUniversal ? 'Universal':'NonUniversal'} React ...`);
       /* eslint-enable no-console */
       clientCompiler.plugin('invalid', (fileName, changeTime) => {
         /* eslint-disable no-console */
@@ -55,6 +55,7 @@ export default function startUniversal(app) {
       });
       activeWebpackDevMiddleware.waitUntilValid((stats) => {
         webpackParseStatsForDepProblems(stats);
+        console.log(`🦁 Webpack development done for ${isUniversal ? 'Universal':'NonUniversal'} React`);
       });
       app.use(activeWebpackDevMiddleware)
       app.use(webpackHotMiddleware(clientCompiler))
@@ -65,39 +66,16 @@ export default function startUniversal(app) {
         })
       )
     });
-  } else if (argv.isDeploy === 'true') {
-    // `npm run bern4`
-
-    // instead of `require` or `__non_webpack_require__` or `import (...)`
-    // using `fs.readFileSync` + `_eval`.
-    // why
-    // -  not `require` because we don't want to bundle what we are importing here
-    //    because what we are importing is itself a bundle
-    //    - which might not exist yet
-    //    - which will itself be ES5 safe
-    //    - which is itself huge so we don't want to bundle it here.
-    // -  `__non_webpack_require__` and `import (...)` don't work or too complicated.
-    // const buildServerMainContent = fs.readFileSync(path.resolve(process.cwd(),'./packages/dev_env/universal/buildServer/main.js'));
-    // const serverRender = _eval(buildServerMainContent,true).default;
-    // const clientStats = fs.readJsonSync(path.resolve(process.cwd(),'./packages/dev_env/universal/buildClient/stats.json'));
-    // const clientProdConfig = webpackConfig({isReact:true,isClient:true,isDev:false,isUniversal:true,'xxx':113});
-    // const publicPath = clientProdConfig.output.publicPath
-    // const outputPath = 'packages/dev_env/universal/buildClient';
-    // app.use(publicPath, express.static(outputPath))
-    // app.use(serverRender({ clientStats, outputPath }))    
   } else {
     // `npm run bern3`
     const clientProdConfig = webpackConfig({isReact:true,isClient:true,isDev:false,isUniversal:true,'xxx':113});
     const serverProdConfig = webpackConfig({isReact:true,isClient:false,isDev:false,isUniversal:true,'xxx':112});
     deleteFiles(`{${clientProdConfig.output.path},${serverProdConfig.output.path}}`, () => {
       const multiCompiler = webpack([clientProdConfig,serverProdConfig]);
+      console.info('🔷 Starting webpack production for Universal React ...');
       webpackRunCompiler(multiCompiler).then(() => {
-        const publicPath = clientProdConfig.output.publicPath
-        const outputPath = clientProdConfig.output.path
-        const serverRender = __non_webpack_require__(res('./universal/buildServer/main.js')).default
-        // const clientStats = __non_webpack_require__(res('./universal/buildClient/stats.json')) 
-        // app.use(publicPath, express.static(outputPath))
-        // app.use(serverRender({ clientStats, outputPath }))
+        console.log('🦁 Webpack production done for Universal React');
+        __non_webpack_require__(res('./universal/buildServer/main.js')).default
       });
     });
   }
