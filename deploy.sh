68c7b09747a29050cc73e34529d47248e7efa359
Brian Ephraim <brianephraim@gmail.com>
2017-09-27 19:22:02 -0400

    deployed 9bd650c

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,8 @@
-echo 1
-echo 2
-git status
-echo 3
-git status
-
 if git status --porcelain | grep . -qs; then
     echo 'Cannot deploy until changes committed.'
 else
     echo 'Repo is clean. Deploy proceeding.'
-    commitHash=$(git rev-parse --short=7 HEAD)
+    commitHash=$(git log --pretty=format:'%h' -n 1)
     branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
     echo "DEPLOYING HASH: $commitHash"
     echo "ORIGINAL BRANCH: $branch"
@@ -16,10 +10,10 @@ else
     git checkout deploy 2>/dev/null || git checkout -b deploy
     # checkout the branch where you started
     git checkout $branch
+    git branch -D deploy-temp1
+    git checkout -b deploy-temp1
     # move your commit over to the deploy branch with message referring to the commit's original hash
     git reset deploy
-    git branch -D deploy-temp
-    git checkout -b deploy-temp
     # collect all the deps in all nested folders, and use to create master deps list for package.json
     # necessary because deploy bundle has external references to node_modules contents
     ./packages/npm-deps/bin/npm-deps < package.json > package-combined.json --silent
@@ -30,8 +24,8 @@ else
     git add -A .
     git commit -m "deployed $commitHash"
     git checkout deploy
-    git rebase deploy-temp
-    git branch -D deploy-temp
+    git rebase deploy-temp1
+    git branch -D deploy-temp1
     git push -u origin deploy
     git checkout $branch
     echo "DONE DEPLOYING, RETURNING TO BRANCH: $branch"
