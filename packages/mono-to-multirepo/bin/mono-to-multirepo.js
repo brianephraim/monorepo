c4741a44dd3244b27d19e964ad1f02fda5a01253
Brian Ephraim <brianephraim@gmail.com>
2017-09-28 11:32:43 -0400

    change __xdirname to __dirnameWhenCompiled

--- packages/mono-to-multirepo/bin/mono-to-multirepo.js
+++ packages/mono-to-multirepo/bin/mono-to-multirepo.js
@@ -5,8 +5,8 @@ const path = require('path');
 const shellCommand = require('@defualt/dev_env/core/shellCommand');
 const isWithinMonoRepo = require('@defualt/dev_env/core/isWithinMonoRepo');
 
-if (isWithinMonoRepo(__xdirname)) {
-  const command = `./node_modules/.bin/babel-node ${path.resolve(__xdirname, '../mono-to-multirepo.js')} ${process.argv.slice(2).join(' ')}`;
+if (isWithinMonoRepo(__dirnameWhenCompiled)) {
+  const command = `./node_modules/.bin/babel-node ${path.resolve(__dirnameWhenCompiled, '../mono-to-multirepo.js')} ${process.argv.slice(2).join(' ')}`;
   shellCommand(command);
 } else {
   require('../dist/mono-to-multirepo');
