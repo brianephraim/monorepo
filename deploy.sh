5fb75e51a65780e8fb74c9500de13a1c733f04c2
Brian Ephraim <brianephraim@gmail.com>
2017-09-28 08:54:25 -0400

    cleanup deploy script especailly with gitignore flexibility

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,50 @@
 if git status --porcelain | grep . -qs; then
-    echo 'Cannot deploy until changes committed.'
+    echo 'DEPLOY SCIPT: Cannot deploy until changes committed.'
 else
-    echo 'Repo is clean. Deploy proceeding.'
+    echo 'DEPLOY SCIPT: Repo is clean. Deploy proceeding.'
     commitHash=$(git log --pretty=format:'%h' -n 1)
     branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-    echo "DEPLOYING HASH: $commitHash"
-    echo "ORIGINAL BRANCH: $branch"
+    echo "DEPLOY SCIPT: DEPLOYING HASH: $commitHash"
+    echo "DEPLOY SCIPT: ORIGINAL BRANCH: $branch"
     # create `deploy` branch if it doesn't exist
+    echo "DEPLOY SCIPT: CREATE DEPLOY BRANCH IF IT DOES NOT EXIST"
     git checkout deploy 2>/dev/null || git checkout -b deploy
     # checkout the branch where you started
     git checkout $branch
+    echo "DEPLOY SCIPT: CREATE/CHECKOUT CLEAN DEPLOY-TEMP BRANCH"
     git branch -D deploy-temp
     git checkout -b deploy-temp
     # move your commit over to the deploy branch with message referring to the commit's original hash
+    echo "DEPLOY SCIPT: RESET CURRENT FILES ON TOP OF DEPLOY BRANCH HISTORY"
     git reset deploy
     # collect all the deps in all nested folders, and use to create master deps list for package.json
     # necessary because deploy bundle has external references to node_modules contents
+    echo "DEPLOY SCIPT: AGGREGATE NESTED PACKAGE.JSON DEPS INTO ROOT LEVEL PACKAGE.JSON"
     ./packages/npm-deps/bin/npm-deps < package.json > package-combined.json --silent
     node ./packages/npm-deps/transfer-deps.js ./package-combined.json ./package.json
     rm ./package-combined.json
     # compile the bundle
-    echo "BUNDLING: $branch"
+    echo "DEPLOY SCIPT: BUNDLING IN DEPLOY-TEMP... ORIGNAL BRANCH WAS: $branch"
     ./packages/dev_env/bin/devenv.js --servers='bernieserver,junk-express' --isDev=false --isUniversal=true --isDeploy=true
+    # b_NO_IGNORE_buildClient
+    # b_NO_IGNORE_buildServer
+    echo "DEPLOY SCIPT: MODIFYING GITIGNORE TO ALLOW COMMIT OF BUNDLES TO DEPLOY-TEMP / DEPLOY BRANCHES"
+    sed -i '' -e 's/buildClient/b_NO_IGNORE_uildClient/g' ./.gitignore
+    sed -i '' -e 's/buildServer/b_NO_IGNORE_uildServer/g' ./.gitignore
+    echo "DEPLOY SCIPT: COMMITTING BUNDLE AND GITIGNORE TO DEPLOY-TEMP"
     git add -A .
     git commit -m "deployed $commitHash"
+    echo "DEPLOY SCIPT: REBASING DEPLOY-TEMP ONTO DEPLOY"
     git checkout deploy
     git rebase deploy-temp
+    echo "DEPLOY SCIPT: DELETING DEPLOY-TEMP"
     git branch -D deploy-temp
+    echo "DEPLOY SCIPT: PUSHING DEPLOY"
     git push -u origin deploy
+    echo "DEPLOY SCIPT: DONE DEPLOYING, RETURNING TO BRANCH: $branch"
     git checkout $branch
-    echo "DONE DEPLOYING, RETURNING TO BRANCH: $branch"
+    echo "DEPLOY SCIPT: WATCHING HEROKU TAIL FOR DEPLOYMENT"
+    echo "---"
+    echo "---"
+    heroku logs --app berniereact --tail
 fi
