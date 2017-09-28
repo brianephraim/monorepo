fb2d9810b650ea3a037b4f8b7359ce866def06a4
Brian Ephraim <brianephraim@gmail.com>
2017-09-28 08:29:56 -0400

    removing some generated files

---
+++ packages/dev_env/universal/buildClient/NotFound.js
@@ -0,0 +1,32 @@
+webpackJsonp([5],{
+
+/***/ 169:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = __webpack_require__(2);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _Switcher = __webpack_require__(159);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+  return _react2.default.createElement(
+    'div',
+    { className: _Switcher.notFound },
+    'PAGE NOT FOUND - 404'
+  );
+};
+
+/***/ })
+
+});
+//# sourceMappingURL=NotFound.js.map
