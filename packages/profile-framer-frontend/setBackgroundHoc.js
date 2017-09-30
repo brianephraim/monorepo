188065630dd37e1dfd30a74ee86b2b64d845a6c9
Brian Ephraim <brianephraim@gmail.com>
2017-09-30 10:40:26 -0400

    loading view progress

--- packages/profile-framer-frontend/setBackgroundHoc.js
+++ packages/profile-framer-frontend/setBackgroundHoc.js
@@ -6,85 +6,98 @@ import ancestorConstantsHoc from './ancestorConstantsHoc';
 
 export default function setBackgroundHoc(Comp) {
   return ancestorConstantsHoc(
-    appConnect(null, {
-      setBackground: (imgSrc,attemptId, ownProps) => {
-        console.log(imgSrc,attemptId);
-        imgSrc = typeof imgSrc === 'object' ? imgSrc.src : imgSrc;
-        return (dispatch, getState) => {
-          return getNormalizedImageInfo(
-            imgSrc,
-            ownProps.constants.backendApiPrefix
-          ).then(response => {
-            const action = payloadRefineAction(
-              {
-                type: 'CROP',
-                payload: {
-                  ...compositeImageIntoParams(
-                    getState()[ownProps.constants.appNameSpace]
-                      .compositeImageData
-                  ),
-                  bgSrcKey: response.srcKey,
-                },
-              },
-              ownProps.constants.appNameSpace
-            );
-            dispatch(action);
-            dispatch({
-              type: 'STOP_LOADING',
-              where: `setBackgroundHoc_${attemptId}`,
+    appConnect(
+      // (appState) => {
+      //   return {}
+      // },
+      null,
+      {
+        setBackground: (imgSrc,attemptId, ownProps) => {
+          console.log(imgSrc,attemptId);
+          imgSrc = typeof imgSrc === 'object' ? imgSrc.src : imgSrc;
+          return (dispatch, getState) => {
+            return getNormalizedImageInfo(
+              imgSrc,
+              ownProps.constants.backendApiPrefix
+            ).then(response => {
+              console.log('getState',getState().loading.filter((item) => { return item === `setBackgroundHoc_${attemptId}`}));
+              const stillLoading = getState().loading.filter((item) => { return item === `setBackgroundHoc_${attemptId}`}).length;
+              if (stillLoading) {
+                const action = payloadRefineAction(
+                  {
+                    type: 'CROP',
+                    payload: {
+                      ...compositeImageIntoParams(
+                        getState().compositeImageData
+                      ),
+                      bgSrcKey: response.srcKey,
+                    },
+                  },
+                  ownProps.constants.appNameSpace
+                );
+                dispatch(action);
+                dispatch({
+                  type: 'STOP_LOADING',
+                  where: `setBackgroundHoc_${attemptId}`,
+                });
+              }
             });
-          });
-        };
-      },
-      setBackgroundTemplateUploader: (imgSrc,attemptId, ownProps) => {
-        imgSrc = typeof imgSrc === 'object' ? imgSrc.src : imgSrc;
-        return (dispatch, getState) => {
-          return getNormalizedImageInfo(
-            imgSrc,
-            ownProps.constants.backendApiPrefix
-          ).then(response => {
-            const compositeImageData = {...getState()[ownProps.constants.appNameSpace].compositeImageData};
-            const action = payloadRefineAction(
-              {
-                type: 'UPLOAD_TEMPLATE',
-                payload: {
-                  ...compositeImageIntoParams(
-                    compositeImageData
-                  ),
-                  bgSrcKey: response.srcKey,
-                  // the foreground will be hidden, so this is a spare parameter.
-                  // lets cache the previous background image here, since it was overwritten for the new template image.
-                  fgSrcKey: compositeImageData.background.srcKey,
+          };
+        },
+        setBackgroundTemplateUploader: (imgSrc,attemptId, ownProps) => {
+          imgSrc = typeof imgSrc === 'object' ? imgSrc.src : imgSrc;
+          return (dispatch, getState) => {
+            return getNormalizedImageInfo(
+              imgSrc,
+              ownProps.constants.backendApiPrefix
+            ).then(response => {
+              const compositeImageData = {...getState().compositeImageData};
+              const action = payloadRefineAction(
+                {
+                  type: 'UPLOAD_TEMPLATE',
+                  payload: {
+                    ...compositeImageIntoParams(
+                      compositeImageData
+                    ),
+                    bgSrcKey: response.srcKey,
+                    // the foreground will be hidden, so this is a spare parameter.
+                    // lets cache the previous background image here, since it was overwritten for the new template image.
+                    fgSrcKey: compositeImageData.background.srcKey,
+                  },
                 },
-              },
-              ownProps.constants.appNameSpace
-            );
-            dispatch(action);
-            dispatch({
-              type: 'STOP_LOADING',
-              where: `setBackgroundHoc_${attemptId}`,
+                ownProps.constants.appNameSpace
+              );
+              dispatch(action);
+              dispatch({
+                type: 'STOP_LOADING',
+                where: `setBackgroundHoc_${attemptId}`,
+              });
             });
-          });
-        };
+          };
+        },
+        onLoading:(attemptId) => {
+          console.log('onloading',attemptId);
+          return (dispatch, getState) => {
+            dispatch({
+              type: 'LOADING',
+              where: `setBackgroundHoc_${attemptId}`
+            })
+          };
+        },
+        onError:(attemptId) => {
+          console.log('onloading',attemptId);
+          return (dispatch, getState) => {
+            dispatch({
+              type: 'LOADINGERROR',
+              where: `setBackgroundHoc_${attemptId}`
+            })
+          };
+        },
       },
-      onLoading:(attemptId) => {
-        console.log('onloading',attemptId);
-        return (dispatch, getState) => {
-          dispatch({
-            type: 'LOADING',
-            where: `setBackgroundHoc_${attemptId}`
-          })
-        };
-      },
-      onError:(attemptId) => {
-        console.log('onloading',attemptId);
-        return (dispatch, getState) => {
-          dispatch({
-            type: 'LOADINGERROR',
-            where: `setBackgroundHoc_${attemptId}`
-          })
-        };
-      },
-    })(Comp)
+      // (stateProps, dispatchProps, ownProps) => {
+      //   console.log('stateProps, dispatchProps, ownProps',stateProps, dispatchProps, ownProps)
+      //   return Object.assign({}, ownProps, stateProps, dispatchProps);
+      // }
+    )(Comp)
   );
 }
--- packages/profile-framer-frontend/setup.js
+++ packages/profile-framer-frontend/setup.js
@@ -286,6 +286,7 @@ export default function(constants) {
       }
       return state;
     },
+    // canceledLoads:
   };
 
   const nameSpacedResponsiveStatusesDictReducer = makeNameSpacedResponsiveStatusesDictReducer(
