49492873fde74b9d32fb22a31eb0c76e304ba839
Brian Ephraim <brianephraim@gmail.com>
2017-10-12 18:54:28 -0400

    small deploy script tweak

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,7 @@ else
     rm ./package-combined.json
     # compile the bundle
     echo "DEPLOY SCIPT: BUNDLING IN DEPLOY-TEMP... ORIGNAL BRANCH WAS: $branch"
-    ./packages/dev_env/bin/devenv.js --servers='./packages/bernieserver/bernieserver.express.js,./packages/junk-express/junk-express.express.js' --isDev=false --isUniversal=true --isDeploy=true
+    npm run just-bundle-prod
     # b_NO_IGNORE_buildClient
     # b_NO_IGNORE_buildServer
     echo "DEPLOY SCIPT: MODIFYING GITIGNORE TO ALLOW COMMIT OF BUNDLES TO DEPLOY-TEMP / DEPLOY BRANCHES"
