fd0b03e2a95d9e5155d62fb2c5c8c9537fd788bd
Brian Ephraim <brianephraim@gmail.com>
2017-10-26 16:27:54 -0400

    apolloClientHoc

--- packages/apollo-simple-starter/client/components/TodaApollo.js
+++ packages/apollo-simple-starter/client/components/TodaApollo.js
@@ -1,123 +1,95 @@
-import React, {Component} from 'react';
-import { gql, graphql, compose } from 'react-apollo';
+import React from 'react';
+import { gql } from 'react-apollo';
 
 import Form from './Form';
 import List from './List';
+import apolloClientHoc from './apolloClientHoc';
 
-function makeThing() {
-  const todaApolloListQuery = gql`
-    query todaApollos {
-      todaApollos {
-        id
-        text
-      }
-    }
-  `;
-
-  // const gqlActions = {
-  //   todaApolloListQuery: {
-  //     gql: `
-  //       query todaApollos {
-  //         todaApollos {
-  //           id
-  //           text
-  //         }
-  //       }
-  //     `
-  //   }
-  // };
-  // gqlActions.todaApolloListQuery.gql
-
-  class TodaApollo extends Component {
-    constructor(){
-      super()
-      this.removeTodaApollo = this.removeTodaApollo.bind(this);
-      this.addTodaApollo = this.addTodaApollo.bind(this);
-    }
-  	removeTodaApollo(todaApollo) {
-  		return this.props.removeTodaApolloMutation({
-  			variables: {
-  				id: todaApollo.id
-  			},
-  			optimisticResponse: {
-  				removeTodaApollo: {
-  					__typename: 'TodaApollo',
-  					text: todaApollo.text
-  				}
-  			},
-  			update: (store, { data: { removeTodaApollo } }) => {
-  				// Read the data from our cache for this query
-  				const data = store.readQuery({ query: todaApolloListQuery });
-  				// Remove our TodaApollo from the mutation to the end
-  				data.todaApollos.splice(data.todaApollos.findIndex(obj => obj.id === todaApollo.id), 1);
-  				// Write our data back to the cache.
-  				store.writeQuery({ query: todaApolloListQuery, data });
-  			}
-  		});
-  	}
-
-  	addTodaApollo(text){
-  		return this.props.addTodaApolloMutation({
-  			variables: {
-  				text
-  			},
-  			optimisticResponse: {
-  				addTodaApollo: {
-  					__typename: 'TodaApollo',
-  					id: Math.random(),
-  					text
-  				}
-  			},
-  			update: (store, { data: { addTodaApollo } }) => {
-  				// Read the data from our cache for this query
-  				const data = store.readQuery({ query: todaApolloListQuery });
-  				// Add our TodaApollo from the mutation to the end
-  				data.todaApollos.push(addTodaApollo);
-  				// Write our data back to the cache.
-  				store.writeQuery({ query: todaApolloListQuery, data });
-  			}
-  		})
-  	}
-    render() {
-    	return (
-        <div className="todaApollo">
-          <Form onSubmit={this.addTodaApollo} />
-          <List data={this.props.todaApolloListQuery.todaApollos} loading={this.props.todaApolloListQuery.loading} onClick={this.removeTodaApollo} />
-        </div>
-      );
-    }
-  };
-
-
-  const removeTodaApolloMutation = gql`
-  	mutation removeTodaApollo($id: ID!) {
-  		removeTodaApollo(id: $id) {
-  			text
-  		}
-  	}
-  `;
-
-  const addTodaApolloMutation = gql`
-  	mutation addTodaApollo($text: String!) {
-  		addTodaApollo(text: $text) {
-  			id
-  			text
-  		}
-  	}
-  `;
-
-  return compose(
-    graphql(removeTodaApolloMutation, {
-      name : 'removeTodaApolloMutation'
-    }),
-    graphql(addTodaApolloMutation, {
-      name: 'addTodaApolloMutation'
-    }),
-    graphql(todaApolloListQuery, {
-      name : 'todaApolloListQuery'
-    }),
-  )(TodaApollo)
+function TodaApollo(props) {
+  return (
+    <div className="todaApollo">
+      <Form onSubmit={props.addTodaApolloMutation} />
+      <List data={props.todaApolloListQuery.todaApollos} loading={props.todaApolloListQuery.loading} onClick={props.removeTodaApolloMutation} />
+    </div>
+  );
 }
 
-export default makeThing();
+const gqlActions = {
+  todaApolloListQuery: {
+    gql: gql`
+      query todaApollos {
+        todaApollos {
+          id
+          text
+        }
+      }
+    `
+  },
+  removeTodaApolloMutation: {
+    gql: gql`
+      mutation removeTodaApollo($id: ID!) {
+        removeTodaApollo(id: $id) {
+          text
+        }
+      }
+    `,
+    configMethod: function configMethod(todaApollo){
+      return {
+        variables: {
+          id: todaApollo.id
+        },
+        optimisticResponse: {
+          removeTodaApollo: {
+            __typename: 'TodaApollo',
+            text: todaApollo.text
+          }
+        },
+        update: (store, { data: { removeTodaApollo } }) => {
+          console.log('remove')
+          // Read the data from our cache for this query
+          const data = store.readQuery({ query: gqlActions.todaApolloListQuery.gql });
+          // Remove our TodaApollo from the mutation to the end
+          data.todaApollos.splice(data.todaApollos.findIndex(obj => obj.id === todaApollo.id), 1);
+          // Write our data back to the cache.
+          store.writeQuery({ query: gqlActions.todaApolloListQuery.gql, data });
+        }
+      };
+    },
+  },
+  addTodaApolloMutation: {
+    gql: gql`
+      mutation addTodaApollo($text: String!) {
+        addTodaApollo(text: $text) {
+          id
+          text
+        }
+      }
+    `,
+    configMethod: function configMethod(text){
+      return {
+        variables: {
+          text
+        },
+        optimisticResponse: {
+          addTodaApollo: {
+            __typename: 'TodaApollo',
+            id: Math.random(),
+            text
+          }
+        },
+        update:  (store, { data: { addTodaApollo } }) => {
+          console.log('add')
+          // Read the data from our cache for this query
+          const data = store.readQuery({ query: gqlActions.todaApolloListQuery.gql });
+          // Add our TodaApollo from the mutation to the end
+          data.todaApollos.push(addTodaApollo);
+          // Write our data back to the cache.
+          store.writeQuery({ query: gqlActions.todaApolloListQuery.gql, data });
+        }
+      };
+    },
+  },
+};
+
+export default apolloClientHoc(TodaApollo, gqlActions);
 // export default graphql(removeTodaApolloMutation)(TodaApollo);
---
+++ packages/apollo-simple-starter/client/components/apolloClientHoc.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import { graphql, compose } from 'react-apollo';
+
+function apolloClientHoc(Comp, gqlActions) {
+
+  class Wrapper extends Component {
+    constructor(){
+      super();
+      
+      this.methods = Object.keys(gqlActions).reduce((accum,actionKey) => {
+        const gqlAction = gqlActions[actionKey];
+        if (gqlAction.configMethod) {
+          const configMethod = gqlAction.configMethod.bind(this)
+          accum[actionKey] = (...args) => {
+            return this.props[actionKey](configMethod(...args));
+          };
+        }
+        return accum;
+      },{})
+    }
+
+    render() {
+      return (
+        <Comp {...this.props} {...this.methods} />
+      );
+    }
+  };
+
+
+  return compose(
+    ...(Object.keys(gqlActions).reduce((accum,actionKey) => {
+      const gqlAction = gqlActions[actionKey];
+      accum.push(graphql(gqlAction.gql, {
+        name: actionKey,
+      }))
+      return accum;
+    },[]))
+  )(Wrapper)
+  // return compose(
+  //   graphql(gqlActions.removeTodaApolloMutation.gql, {
+  //     name : 'removeTodaApolloMutation'
+  //   }),
+  //   graphql(gqlActions.addTodaApolloMutation.gql, {
+  //     name: 'addTodaApolloMutation'
+  //   }),
+  //   graphql(gqlActions.todaApolloListQuery.gql, {
+  //     name : 'todaApolloListQuery'
+  //   }),
+  // )(Wrapper)
+}
+export default apolloClientHoc;
